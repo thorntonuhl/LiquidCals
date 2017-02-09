ace182571f837b26fae1443f17048ff051183b44
Ethan Park <cpraf104@gmail.com>
2017-02-09 12:18:44 -0600

    Second iteration of javascript
    Need to get specific names and attributes (such as image height, slider
    height, stuff like that

--- js/script.js
+++ js/script.js
@@ -3,16 +3,27 @@
 //EECS 330
 //Colin Pham, Thornton Uhl, Matthew Griswold, Ethan Park
 
-var container = ""; 
+var curr = 1;
+
 var caloriedb = {};
-var drink = "";
 var calories = 0;
-var height = 0;
-var amount = 0;
-var firstpg = document.getElementById('home');
-var secondpg = document.getElementById('secondpg'); //Adjust name as needed
-var thirdpag = document.getElementById('thirdpg');  //Adjust name as needed
 var next = document.getElementById('next');         //Adjust name as needed (for next button)
+
+var firstpg = document.getElementById('home');
+var wineglass = document.getElementById('wine glass');
+var coffee = document.getElementById('coffee cup');
+var shotglass = document.getElementById('shot glass');
+var cup = document.getElementById('plastic cup');
+var container = "";
+
+var secondpg = document.getElementById('secondpg'); //Adjust name as needed
+var drink = "";
+
+var thirdpag = document.getElementById('thirdpg');  //Adjust name as needed
+var height = document.getElementById('drinkcontainer');
+var amount = document.getElementById('slider');
+var answer = 0;
+
 var drinks = document.getElementsByClassName('container');
 var leftShiftDrinks = document.getElementById('left-shift');
 var rightShiftDrinks = document.getElementById('right-shift');
@@ -37,6 +48,13 @@ function display(page) {
   }
 } 
 
+function choosecontainer(ccontainer) {
+  if (ccontainer == 'wine glass') container = "wine glass";
+  else if (ccontainer == 'coffee cup') container = "coffee cup";
+  else if (ccontainer == 'shot glass') container = "shot glass";
+  else if (ccontainer == 'plastic cup') container = "plastic cup";
+}
+
 function shiftDrinksLeft() {
   selected_drinks = selected_drinks.map(function(el) {
     if (el == 0) {
@@ -57,9 +75,29 @@ function shiftDrinksRight() {
   console.log(selected_drinks);
 }
 
-display(firstpg);
-next.onclick = display(secondpg);
-next.onclick = display(thirdpg);
+if (curr == 1) {//FIRST PAGE
+  display(firstpg);
+  rightShiftDrinks.onclick = shiftDrinksRight;
+  leftShiftDrinks.onclick = shiftDrinksLeft;
+  wineglass.onclick = choosecontainer('wine glass'); //Wine glass is chosen
+  coffee.onclick = choosecontainer('coffee cup');    //Coffee cup is chosen
+  shotglass.onclick = choosecontainer('shot glass'); //Shot glass is chosen
+  plastic.onclick = choosecontainer('plastic cup');  //Plastic cup is chosen
+  next.onclick = display(secondpg);
+  curr = 2;
+}
 
-rightShiftDrinks.onclick = shiftDrinksRight;
-leftShiftDrinks.onclick = shiftDrinksLeft;
+else if (curr == 2) {//SECOND PAGE
+  drink = ti;
+  for (name in caloriedb){
+    if(name == drink) {
+      calories = caloriedb[name];
+    }
+  }
+  next.onclick = display(thirdpg);
+  curr = 3;
+}
+
+else {
+  answer = (height / amount) * calories;
+}
