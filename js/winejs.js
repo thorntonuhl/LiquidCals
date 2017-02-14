6af1ba832f7c5bd7b74589dc48c1a822ded7a071
Collin Pham <collinpham2018@u.northwestern.edu>
2017-02-13 20:07:26 -0600

    style updates:

--- js/winejs.js
+++ js/winejs.js
@@ -10,7 +10,7 @@ $("#thirdpg").click(function(event){
     fillpercent = ((($(".fill").height() + $(".fill").offset().top) - event.pageY) / $(".fill").height());
 	if (fillpercent > 0 && fillpercent < 1) {
 		$(".st1").css("-webkit-clip-path", "polygon(0 " + ((1-fillpercent)*100) + "%, 100% " + ((1-fillpercent)*100) + "%, 100% 100%, 0% 100%)");
-		$("#text").text("Percent of cup full: " + (fillpercent.toFixed(3)*100) + "%. We can use this variable to calculate calories");
+		$("#text").text("Calories in drink: " + (fillpercent.toFixed(3)*100).toFixed(1));
 }
 });
 
@@ -27,7 +27,7 @@ $("#thirdpg").mousemove(function(event){
     fillpercent = ((($(".fill").height() + $(".fill").offset().top) - event.pageY) / $(".fill").height());
 	if (fillpercent > 0 && fillpercent < 1) {
 		$(".st1").css("-webkit-clip-path", "polygon(0 " + ((1-fillpercent)*100) + "%, 100% " + ((1-fillpercent)*100) + "%, 100% 100%, 0% 100%)");
-		$("#text").text("Percent of cup full: " + (fillpercent.toFixed(3)*100) + "%. We can use this variable to calculate calories");
+		$("#text").text("Carories in drink: " + (fillpercent.toFixed(3)*100).toFixed(1));
 }
 	}
 });	
