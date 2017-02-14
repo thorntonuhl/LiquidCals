e2942cd48bda439a58cf4e1f8955dd0f67e0a1a1
Matthew Griswold <matthewgriswold@dhcp-10-105-101-145.wireless.northwestern.private>
2017-02-13 18:35:22 -0600

    added js reset

--- js/winejs.js
+++ js/winejs.js
@@ -1,15 +1,17 @@
 // JavaScript Document
 
-
-$(document).ready(function (e) {
-
+$(document).ready(function () {
+"use strict";
 var isdragging = false;	
 var fillpercent = 1;
+
 	
 $("#svgcontainer").click(function(event){
     fillpercent = ((($(".fill").height() + $(".fill").offset().top) - event.pageY) / $(".fill").height());
+	var sizer = $(".fill")[0].getBoundingClientRect();
+	alert("height: " + sizer.height() + " offset: " + $(".fill").offset().top + " pageY: " + event.pageY);
 	if (fillpercent > 0 && fillpercent < 1) {
-		$(".st1").css("clip-path", "polygon(0 " + ((1-fillpercent)*100) + "%, 100% " + ((1-fillpercent)*100) + "%, 100% 100%, 0% 100%)");
+		$(".st1").css("-webkit-clip-path", "polygon(0 " + ((1-fillpercent)*100) + "%, 100% " + ((1-fillpercent)*100) + "%, 100% 100%, 0% 100%)");
 		$("#text").text("Percent of cup full: " + (fillpercent.toFixed(3)*100) + "%. We can use this variable to calculate calories");
 }
 });
@@ -26,11 +28,15 @@ $("#svgcontainer").mousemove(function(event){
 	if (isdragging) {
     fillpercent = ((($(".fill").height() + $(".fill").offset().top) - event.pageY) / $(".fill").height());
 	if (fillpercent > 0 && fillpercent < 1) {
-		$(".st1").css("clip-path", "polygon(0 " + ((1-fillpercent)*100) + "%, 100% " + ((1-fillpercent)*100) + "%, 100% 100%, 0% 100%)");
+		$(".st1").css("-webkit-clip-path", "polygon(0 " + ((1-fillpercent)*100) + "%, 100% " + ((1-fillpercent)*100) + "%, 100% 100%, 0% 100%)");
 		$("#text").text("Percent of cup full: " + (fillpercent.toFixed(3)*100) + "%. We can use this variable to calculate calories");
 }
 	}
 });	
 
 });
-				  
+	
+function reset () {
+	"use strict";
+	$(".st1").css("-webkit-clip-path", "polygon(0 0%, 100% 0%, 100% 100%, 0% 100%)");
+}
